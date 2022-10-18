fcace534c8ab11884ab144b81566be98d6ca5e8c
cxy205 <73516636+cxy205@users.noreply.github.com>
2022-10-19 00:56:21 +0800

    Update Q6.sql

--- 作业1/Q6.sql
+++ 作业1/Q6.sql
@@ -1,5 +1,6 @@
 select id,orderdate,case when preorderdate<>0 then preorderdate else orderdate end,round((julianday(orderdate)-julianday(case when preorderdate<>0 then preorderdate else orderdate end)),2)
-from (select id,orderdate,lag(orderdate,1,0) over (order by orderdate)preorderdate
-from 'order'
-where customerid='BLONP')
+from (
+  select id,orderdate,lag(orderdate,1,0) over (order by orderdate)preorderdate
+  from 'order'
+  where customerid='BLONP')
 limit 10;
