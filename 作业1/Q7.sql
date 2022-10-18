53d5d109cfe21cc6636459b2244647ebadd4b73a
cxy205 <73516636+cxy205@users.noreply.github.com>
2022-10-19 00:57:10 +0800

    Update Q7.sql

--- 作业1/Q7.sql
+++ 作业1/Q7.sql
@@ -1,6 +1,7 @@
 select customerid,comname,summ
-from (select customerid,ifnull(companyname,'MISSING_NAME') as comname,sum(unitprice*quantity) as summ,NTILE (4) OVER ( ORDER BY sum(unitprice*quantity) ) num
-from orderdetail left join ('order' left join customer on 'order'.customerid=customer.id) on orderdetail.orderid='order'.id
-group by customerid
-order by sum(unitprice*quantity))
+from (
+  select customerid,ifnull(companyname,'MISSING_NAME') as comname,sum(unitprice*quantity) as summ,NTILE (4) OVER ( ORDER BY sum(unitprice*quantity) ) num
+  from orderdetail left join ('order' left join customer on 'order'.customerid=customer.id) on orderdetail.orderid='order'.id
+  group by customerid
+  order by sum(unitprice*quantity))
 where num=1;
