4d3ec28272c8665176ad98d457d243ce7bd49056
cxy205 <73516636+cxy205@users.noreply.github.com>
2022-10-19 00:57:55 +0800

    Update Q8.sql

--- 作业1/Q8.sql
+++ 作业1/Q8.sql
@@ -1,7 +1,7 @@
 select t.regiondescription,t.firstname,t.lastname,t.birthdate
 from(
-Select *,row_number() over (partition by regiondescription order by birthdate desc) as n
-from employee,employeeTerritory,region,territory
-where region.id=territory.regionid and territory.id=employeeterritory.territoryid and employeeterritory.employeeid=employee.id)t
+  Select *,row_number() over (partition by regiondescription order by birthdate desc) as n
+  from employee,employeeTerritory,region,territory
+  where region.id=territory.regionid and territory.id=employeeterritory.territoryid and employeeterritory.employeeid=employee.id)t
 where t.n=1
 order by regionid;
